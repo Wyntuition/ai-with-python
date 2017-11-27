133599c8852f0b468dcfd64cc46024a973875244
wyntuition <wynvandev@gmail.com>
2017-11-26 22:55:07 -0500

    org

--- 01-preprossing-data/preprocessing-data.py
+++ 01-preprossing-data/preprocessing-data.py
@@ -1,6 +1,7 @@
 import numpy as np 
 from sklearn import preprocessing 
 
+# Some random sample data
 input_data = np.array([[5.1, -2.9, 3.3], 
                        [-1.2, 7.8, -6.1], 
                        [3.9, 0.4, 2.1], 
